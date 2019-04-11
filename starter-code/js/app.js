9f08d8eb881158e7cef291d7da54362a3b6a31fe
Yuan Gao <example@email.com>
2019-04-11 13:45:44 -0700

    driver is yuan navigator is jen. almost finish catalog

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -8,6 +8,7 @@ var Cart = function(items) {
 
 Cart.prototype.addItem = function(product, quantity) {
   // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  this.items.push(new CartItem(product,quantity));
 };
 
 Cart.prototype.saveToLocalStorage = function() {
